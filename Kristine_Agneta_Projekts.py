73ce008431ad088103077954fc34bec8d5e8886f
barkaane1 <kristinebarkane025@gmail.com>
2025-04-15 14:19:58 +0300

    Update Kristine_Agneta_Projekts.py

--- Kristine_Agneta_Projekts.py
+++ Kristine_Agneta_Projekts.py
@@ -3,10 +3,27 @@ import datetime
 def main():
     print("Programma ļauj apskatīt savus stundu kavējumus un rezultātus redzēt procentuāli.")
     #izveido savienojumu ar db un kursoru kas izpildīs vaicājumus
-    with _sqlite3.connect("Kristine-Agneta_Projekts.db") as conn:
+    with sqlite3.connect("Kristine-Agneta_Projekts.db") as conn:
         c = conn.cursor()
         izvele(c)
-def izvele(c):
+def izvele (ieiet):
+    iz = int(input("Izvēlaties darbību : ieiet profilā - 1, izveidot profilu - 2: "))
+    if iz == 1 :
+        ieiet()
+    elif iz == 2 :
+        izveidot()
+    
+def ieiet(c):
+    liet = input("ievadiet savu lietotājvārdu")
+    c.execute(f"SELECT * WHERE LIETOTAJS = \"{liet} \"")
+def izveidot():
+    vards = input("ievadiet savu vārdu:")
+    uzvards = input ("ievadiet savu uzvārdu: ")
+    lietotajv = input("ievadiet savu lietotājvārdu: ")
+    stundas = input("ievadiet cik stundas jums ir nedēļā: ")
+    kl = ("ievadiet savu klasi: ")
+    c.excute(f"update LIETOTAJS set Vards = \"{vards}\", Uzvards = \"{uzvards}\", Klase = \"{kl}\", Lietv = \"{lietotajv}\"")
+def iespejas(c):
     print("Izvēlies darbību:")
     while True:
         print("1.Pievienot nedēļas apmeklējumu\n2. Aprēķināt apmeklējumu procentu\n3. Aprēķināt aptuveno gada apmeklējumu\n4. Skatīt iepriekšējos datus\n5. Beigt darbu")
@@ -36,3 +53,8 @@ def pievienot(c):
         print(f"Tavs apmeklējums šajā nedēļā ir {kavejums:.2f}%.")
     except ValueError:
         print("Nederīga ievade")
+
+
+
+if __name__ == "__main__":
+    main()
