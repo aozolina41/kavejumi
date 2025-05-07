fe700c9bb178cae3ec3cf05c1c0f83b040b1c175
barkaane1 <kristinebarkane025@gmail.com>
2025-05-07 14:14:48 +0300

    Update Kristine_Agneta_Projekts.py

--- Kristine_Agneta_Projekts.py
+++ Kristine_Agneta_Projekts.py
@@ -11,19 +11,27 @@ def savienojums():
 
 def izvele ():
     iz = int(input("Izvēlaties darbību : ieiet profilā - 1, izveidot profilu - 2: "))
-    if iz == 1 :
-        liet = ieiet()
-        iespejas(liet)
-    elif iz == 2 :
-        izveidot()
+    try:
+        if iz == 1 :
+            liet = ieiet()
+            iespejas(liet)
+        elif iz == 2 :
+            izveidot()
+        else:
+            print("nederīga ievade!! ")
+    except ValueError:
+        print("Lūdzu ievadi tikai skaitli!")
     
 def ieiet():
     c = savienojums()
     liet = input("ievadiet savu lietotājvārdu: ")
-    #c.execute(f"SELECT * from LIETOTAJS WHERE Lietotajv = \"{liet} \"")
-    c.execute(f"SELECT Liet_ID from LIETOTAJS WHERE Lietotajv = \"{liet}\"")
-    d = c.fetchall()[0][0]
-    
+    try:
+        #c.execute(f"SELECT * from LIETOTAJS WHERE Lietotajv = \"{liet} \"")
+        c.execute(f"SELECT Liet_ID from LIETOTAJS WHERE Lietotajv = \"{liet}\"")
+        d = c.fetchall()[0][0]
+    except:
+        print(f"Kļūda pieslēdzoties(lietotājs neeksistē)")
+        return None
     return d
 def izveidot():
     c = savienojums()
@@ -32,49 +40,63 @@ def izveidot():
     lietotajv = input("ievadiet savu lietotājvārdu:  ")
     kl = input("ievadiet savu klasi: ")
     stundas = input("ievadi stundu skaitu nedēļā: ")
-    if c.fetchone():
-        print("Šāds lietotājvārds jau eksistē. Lūdzu, izvēlieties citu.")
+    try:
+        if c.fetchone():
+            print("Šāds lietotājvārds jau eksistē. Lūdzu, izvēlieties citu.")
+            izveidot()
+        c.execute(f"insert into LIETOTAJS (vards, Uzvards, Klase, Lietotajv ) values(\"{vards}\", \"{uzvards}\", {kl}, \"{lietotajv}\"  )")
+        c.connection.commit()
+        liet_ID = c.lastrowid
+        c.execute(f"INSERT INTO STUNDAS (ned_st, liet_ID) VALUES ({stundas}, {liet_ID})")
+        c.connection.commit()
+        iespejas(liet_ID) 
+    except ValueError:
+        print("Stundu skaitam jābūt skaitlim!")
         izveidot()
-    c.execute(f"insert into LIETOTAJS (vards, Uzvards, Klase, Lietotajv ) values(\"{vards}\", \"{uzvards}\", {kl}, \"{lietotajv}\"  )")
-    c.connection.commit()
-    liet_ID = c.lastrowid
-    c.execute(f"INSERT INTO STUNDAS (ned_st, liet_ID) VALUES ({stundas}, {liet_ID})")
-    c.connection.commit()
-    iespejas(liet_ID) 
+    except :
+        print(f"Kļūda izveidojot profilu")
 def iespejas(liet):
     c = savienojums()
     print("Izvēlies darbību:")
-    while True:
-        print("1.Pievienot nedēļas apmeklējumu\n2. apskatīt profilu\n3. Beigt darbu")
-        izvele = input("Tava izvēle: ")
-        if izvele not in ["1", "2","3","4","5"]:
-            print("Nederīga ievade")
-            continue
-        if izvele == "1":
-            pievienot(liet)
-        elif izvele == "2":
-            apskatit(liet)
-        elif izvele == "3":
-            break
+    try:
+        while True:
+            print("1.Pievienot nedēļas apmeklējumu\n2. apskatīt profilu\n3. Beigt darbu")
+            izvele = input("Tava izvēle: ")
+            if izvele not in ["1", "2","3","4","5"]:
+                print("Nederīga ievade")
+                continue
+            if izvele == "1":
+                pievienot(liet)
+            elif izvele == "2":
+                apskatit(liet)
+            elif izvele == "3":
+                break
+    except Exception as e:
+        print(f"Kļūda darbībā: {e}")
 def pievienot(liet):
-    c = savienojums()
-    stundas_kopa = int(input("Ievadi kopējo stundu skaitu nedēļā: "))
-    kavetas_stundas = int(input("Ievadi kavēto stundu skaitu nedēļā: "))
-    apmeklets = stundas_kopa - kavetas_stundas
-    kavejums = apmeklets / stundas_kopa * 100
-    q = f"UPDATE STUNDAS SET Ned_st = {stundas_kopa}, Neapm = {kavetas_stundas} WHERE liet_ID = {liet}"
-    c.execute(q)
-    print(f"Tavs apmeklējums šajā nedēļā ir {kavejums:.2f}%.")
-    c.connection.commit()
-
-
-
+    try:
+        c = savienojums()
+        stundas_kopa = int(input("Ievadi kopējo stundu skaitu nedēļā: "))
+        kavetas_stundas = int(input("Ievadi kavēto stundu skaitu nedēļā: "))
+        apmeklets = stundas_kopa - kavetas_stundas
+        kavejums = apmeklets / stundas_kopa * 100
+        q = f"UPDATE STUNDAS SET Ned_st = {stundas_kopa}, Neapm = {kavetas_stundas} WHERE liet_ID = {liet}"
+        c.execute(q)
+        print(f"Tavs apmeklējums šajā nedēļā ir {kavejums:.2f}%.")
+        c.connection.commit()
+    except ValueError:
+        print("Lūdzu ievadi skaitļus!")
+    except Exception as e:
+        print(f"Kļūda atjauninot datus: {e}")
 def apskatit(liet):
-    c = savienojums()
-    
-    c.execute(f"SELECT * FROM LIETOTAJS WHERE Liet_ID= {liet}")
-    dati = c.fetchone()
-    print(dati)
+    try:
+        c = savienojums()
+        
+        c.execute(f"SELECT * FROM LIETOTAJS WHERE Liet_ID= {liet}")
+        dati = c.fetchone()
+        print(dati)
+    except Exception as e:
+        print(f"Kļūda skatot profilu: {e}") 
 
 
 
